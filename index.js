8c57856bd8c37245754466a8845dc99a93af3fc1
Daxman21 <reiddavin@outlook.com>
2020-11-16 16:28:29 -0500

    🖌Fine tune css styling - Added a function to handle form submissions -Added the newsletter web application to projects section - Tweaked css elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(process.platform);
+onSubmit = () => {
+    let person = {
+        name: $('input.vistorName').val(),
+        email: $('input.vistorEmail').val(),
+        message: $('textarea.vistorMessage').val()
+    }
+    window.open('mailto:reiddain@outlook.com?subject=Message from: ' + person.name + " | At: " + person.email + "&body= " + person.message );
+}
